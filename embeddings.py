4d941ca3eb7a2671810fb636a67a7403d59c5539
Matt Nikonorov <89400357+MattNikonorov@users.noreply.github.com>
2023-12-13 21:12:03 +0100

    Create embeddings.py

---
+++ embeddings.py
@@ -0,0 +1,32 @@
+from langchain.embeddings import OpenAIEmbeddings
+from openai.embeddings_utils import cosine_similarity
+import os
+import pandas
+import time
+
+os.environ["OPENAI_API_KEY"] = "YOUR_OPENAI_KEY"
+embeddings_model = OpenAIEmbeddings()
+
+f = open("texts/Beyond Good and Evil.txt", "r")
+phi_text = str(f.read())
+
+chapters = phi_text.split("CHAPTER")
+
+emb_list = []
+
+for i in range(len(chapters)):
+    embs = embeddings_model.embed_query(chapters[i])
+    emb_list.append(embs)
+    # time.sleep(20) if you face rate limits
+
+embedded_question = embeddings_model.embed_query("What are the flaws of philosophers?")
+similarities = [] 
+similarities2 = [] 
+tags = []
+for i2 in range(len(emb_list)):
+    similarities.append(cosine_similarity(emb_list[i2], embedded_question))
+    similarities2.append(cosine_similarity(emb_list[i2], embedded_question))
+    tags.append(f"CHAPTER {i2}")
+
+similarities.sort()
+print(tags[similarities2.index(similarities[-1])])
